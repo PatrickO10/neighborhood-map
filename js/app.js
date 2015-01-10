fd6ef9dc31c1c568673a769b7facfcc841e3e26a
patricko10 <patrickorth10@gmail.com>
2015-01-10 09:40:28 -0600

    sets up jquery, knockout, and bootstrap. Adds css folder and js folder.

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//TODO: ViewModel
+	/*TODO: call MapView function
+	* Call PlacesList Function
+
+//TODO: View
+	/*TODO: MapView function
+	* Display the map
+	* Display Markers
+	* Display Street View
+	*/
+	/*TODO: PlacesList function
+	* Display list of places in the neighborhood
+	* When clicking on a place transition to upper right
+	* Another box opens on the right taking up the screen
+	* If applicable it will give other information
+	*/
+	//TODO: Weather API
+	//TODO: Wikipedia
+	//TODO: Twitter Feed
+
+//TODO: Model
+	//TODO: Foursquare
+	//TODO: PanoID
+	//TODO:
