4adae50960de46c3a4cf8018ea6fc372c969f26c
patricko10 <patrickorth10@gmail.com>
2015-01-14 10:34:39 -0600

    adds a service search and createMarkers

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 var view = {
 
 };
+
 // The data
 var model = {
 
@@ -28,12 +29,12 @@ var ViewModel = function() {
         googModel.addMarker(googModel.markerOptions); // Data
         googView.setAllMap(map); // Renders on UI
     });
-
 };
+
 ViewModel.prototype = {
     init: function() {
-        googModel.init();
         googView.init(googModel);
+        googModel.init();
         googView.autoRotate();
     },
     doSomething: function() {
@@ -57,32 +58,82 @@ var Food = function(data) {
     this.name = ko.observable(data.name);
 };
 
-
 var map;
 var markers = [];
+var infowindow;
+var service;
+var requestedPlaces = [];
+var uptown = new google.maps.LatLng(44.9519177, -93.2983446);
 var googModel = {
     init: function() {
         var self = this;
         // Adds a marker to marker array
         self.addMarker(self.markerOptions);
+        infowindow = new google.maps.InfoWindow();
+        service = new google.maps.places.PlacesService(map);
+        console.log(self.request);
+        service.nearbySearch(self.request, self.callback);
     },
     mapOptions: {
-        zoom: 17,
-        center: new google.maps.LatLng(44.9519177, -93.2983446),
-        scrollwheel: false,
-        draggable: false,
+        zoom: 15,
+        center: uptown,
+        scrollwheel: true,
+        draggable: true,
         disableDefaultUI: true,
         mapTypeId: google.maps.MapTypeId.ROADMAP //google.maps.MapTypeId.TERRAIN
     },
     markerOptions: {
         map: map,
-        position: new google.maps.LatLng(44.9519177, -93.2983446),
-        icon: 'images/mapmarker.png'
+        position: uptown
+    },
+    request: {
+        location: uptown,
+        radius: '1000',
+        types: ['art_gallery', 'atm', 'bar', 'bowling_alley', 'bakery', 'bank', 'beauty_salon',
+            'bicycle_store', 'book_store', 'cafe', 'clothing_store', 'convenience_store', 'department_store',
+            'food', 'night_club', 'park', 'restaurant', 'shopping_mall', 'spa'
+        ]
     },
     addMarker: function(data) {
         this.markerOptions = data;
         var marker = new google.maps.Marker(this.markerOptions);
         markers.push(marker);
+    },
+    callback: function(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results.length);
+            for (var i = 0; i < results.length; i++) {
+                console.log(results[i]);
+                googModel.createMarker(results[i]);
+            }
+        }
+    },
+    createMarker: function(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: placeLoc,
+            /*icon: {
+                // Star
+                path: 'M 0,-24 6,-7 24,-7 10,4 15,21 0,11 -15,21 -10,4 -24,-7 -6,-7 z',
+                fillColor: '#ffff00',
+                fillOpacity: 1,
+                scale: 1 / 4,
+                strokeColor: '#bd8d2c',
+                strokeWeight: 1
+            }*/
+        });
+        var request = {
+            reference: place.reference
+        };
+        service.getDetails(request, function(details, status) {
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(details.name + "<br />" + details.formatted_address +
+                    "<br />" + details.website + "<br />" + details.rating + "<br />" +
+                    details.formatted_phone_number);
+                infowindow.open(map, this);
+            });
+        });
     }
 };
 var googView = {
@@ -95,6 +146,7 @@ var googView = {
             self.mapOptions);
         // Sets every marker in markers on the map
         self.setAllMap(map);
+
     },
     setAllMap: function(map) {
         for (var i = 0; i < markers.length; i++) {
@@ -113,7 +165,10 @@ var googView = {
     }
 
 };
+
 ko.applyBindings(new ViewModel());
+/*
 $("#map-canvas").click(function() {
     alert("Handler for .click() called.");
 });
+*/
