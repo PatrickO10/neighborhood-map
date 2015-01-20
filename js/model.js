46d64db9c58565c30f9693bfece99a8087320950
patricko10 <patrickorth10@gmail.com>
2015-01-19 18:28:27 -0600

    adds quicksort to ajax call, and sets more items to obs

--- js/model.js
+++ js/model.js
@@ -5,14 +5,33 @@ $(function(my) {
     my.Venue = function(data) {
         var self = this;
         self.name = ko.observable(data.venue.name);
-        self.rating = ko.observable(my.Rating(data.venue.rating));
+        self.rating = ko.observable(my.VenueRating(data.venue.rating));
         self.nameRate = ko.computed(function() {
             return self.name() ? self.name() + " " + self.rating() : "";
         });
+        self.type = ko.observable(data.venue.categories[0].shortName);
+        self.lat = ko.observable(data.venue.location.lat);
+        self.lng = ko.observable(data.venue.location.lng);
+        self.address = ko.observable(data.venue.location.formattedAddress);
+        self.phone = ko.observable(my.VenuePhone(data.venue.contact.formattedPhone));
+        self.tip = ko.observable(my.VenueTip(data.tips));
+        self.web = ko.observable(my.VenueWeb(data.venue.url));
     };
-    my.Rating = function(data) {
-    	console.log((data * 10) % 10);
+
+    my.VenueRating = function(data) {
         return data ? ((data * 10) % 10 === 0 ? data.toFixed(1) : data) : "N/A";
+    };
+
+    my.VenuePhone = function(data) {
+        return data ? data : "No phone";
+    };
+
+    my.VenueTip = function(data) {
+        return data ? data[0].text : "No tips";
+    };
+
+    my.VenueWeb = function(data) {
+        return data ? data : "No URL available";
     };
 
     my.vm = (function() {
@@ -20,26 +39,14 @@ $(function(my) {
         return {
             venueList: venueList
         };
-
-        /*            return {
-                        venues: venues,
-                        name: name,
-                        addNames: function() {
-                            console.log(venues());
-                            venues().forEach(function(nameItem) {
-                                my.vm.name.push(nameItem);
-                            });
-                            console.log(name());
-                        }
-                    };*/
     })();
 
-    function foursqAPI() {
+    my.foursqAjax = function() {
         var prefixUrl = "https://api.foursquare.com/v2/venues/explore?",
             uniqueID = 'client_id=DZIPLZYHXXYLCELWMS3N2DIO35PWEKTIZMABHZQ4VWKAU2JA&client_secret=1BFTWIS2O3IZLCDZNV2R2A4ITV0UYAVJV2MDBXIW3LWUOIOM',
             uptownLL = '&ll=' + 44.9519177 + ',' + -93.2983446,
             section = '&section=' + 'topPlaces', // TODO: observable for search content.
-            suffixUrl = uniqueID + uptownLL + section + '&v=20130815&radius=500&limit=50',
+            suffixUrl = uniqueID + uptownLL + section + '&v=20130815&radius=500&limit=5',
             requestUrl = prefixUrl + suffixUrl;
 
         $.ajax({
@@ -47,12 +54,23 @@ $(function(my) {
             dataType: 'jsonp',
             success: function(data) {
                 var requestedData = data.response.groups[0].items;
-                requestedData.forEach(function(venueData) {
-                    my.vm.venueList.push(new my.Venue(venueData));
+                console.log(requestedData);
+                // Sorts each venue by comparing ratings
+                requestedData.sort(function(a, b) {
+                    if (a.venue.rating < b.venue.rating) {
+                        return 1;
+                    } else if (a.venue.rating > b.venue.rating) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                });
+                requestedData.forEach(function(venueItem) {
+                    my.vm.venueList.push(new my.Venue(venueItem));
                 });
             }
         });
-    }
-    foursqAPI();
+    };
+    my.foursqAjax();
     ko.applyBindings(my.vm);
 });
